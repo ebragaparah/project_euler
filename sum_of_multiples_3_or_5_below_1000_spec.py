d41089f2d93a4ae9cf795f53713962a216e00b77
= <eduardo@eduardo-macbook.local>
2011-07-15 15:54:03 -0300

    alterado idioma dos títulos dos testes.

--- sum_of_multiples_3_or_5_below_1000_spec.py
+++ sum_of_multiples_3_or_5_below_1000_spec.py
@@ -1,18 +1,18 @@
 import unittest
-from should_dsl import should, should_not
+from should_dsl import should
 from sum_of_multiples_3_or_5_below_1000 import *
 
 class MultiplesSpec(unittest.TestCase):
-  def test_deve_informar_que_a_soma_dos_multiplos_de_3_ou_5_ate_6_eh_8(self):
+  def test_the_sum_of_multiples_of_3_or_5_below_6_is_8(self):
     sum_of_multiples_below(6) |should| equal_to(8)
 
-  def test_deve_informar_que_a_soma_dos_multiplos_de_3_ou_5_ate_10_eh_23(self):
+  def test_the_sum_of_multiples_of_3_or_5_below_10_is_23(self):
     sum_of_multiples_below(10) |should| equal_to(23)
 
-  def test_deve_informar_que_a_soma_dos_multiplos_de_3_ou_5_ate_15_eh_45_(self):
+  def test_the_sum_of_multiples_of_3_or_5_below_15_is_45(self):
     sum_of_multiples_below(15) |should| equal_to(45)
 
-  def test_deve_informar_que_a_soma_dos_multiplos_de_3_ou_5_ate_1000_eh_x(self):
+  def test_the_sum_of_multiples_of_3_or_5_below_1000_is_2333168(self):
     sum_of_multiples_below(1000) |should| equal_to(233168)
 
 if __name__ == '__main__':
