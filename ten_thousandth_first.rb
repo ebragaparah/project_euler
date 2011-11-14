a72b284c08ffe3f31256094b714f8da6c5e1f7be
= <ebfjunior@gmail.com>
2011-11-14 01:19:43 -0200

    adicionadas soluções e especificações do sétimo problema

---
+++ ten_thousandth_first.rb
@@ -0,0 +1,29 @@
+include Math
+
+class Fixnum
+	def prime?
+	  divisors = []
+	  case self
+    when 0, 1
+      false
+    else
+	    (1..(sqrt(self.abs).to_i + 1)).each do |iterator|
+	      divisors << iterator if ((self % iterator).zero?) and (self != iterator)
+      end
+      divisors.length <= 1
+    end
+  end
+end
+
+
+def prime_in_position(number)
+  primes = []
+  counter = 0
+  while primes.length <= 10001 do
+    if counter.prime?
+      primes << counter
+    end
+    counter += 1
+  end
+  primes[number]
+end
---
+++ ten_thousandth_first_spec.rb
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+require './first_thousandth_prime'
+
+describe "First thousandth prime number." do
+  context "Verifying if a number is prime." do
+    
+    it "Should return true if number is prime." do
+      0.prime?.should == false
+      1.prime?.should == false
+      2.prime?.should == true
+      3.prime?.should == true
+      -3.prime?.should == true
+      13.prime?.should == true
+      14.prime?.should == false
+      -14.prime?.should == false
+      15.prime?.should == false
+      997.prime?.should == true
+      -997.prime?.should == true
+    end
+  end
+  
+  context "Find the first thousandth prime number." do
+    it "Should return the prime if its position in primes array is correct." do
+      prime_in_position(0).should == 2
+      prime_in_position(1).should == 3
+      prime_in_position(2).should == 5
+      prime_in_position(3).should == 7
+      prime_in_position(4).should == 11
+      prime_in_position(9).should == 29
+      prime_in_position(1000).should == 7927
+      prime_in_position(10000).should == 104743
+    end
+  end
+end
