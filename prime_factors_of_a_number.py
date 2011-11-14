cf5c59141bab0c454a58dea47f1c0219ac8f1904
= <ebfjunior@gmail.com>
2011-11-14 01:33:29 -0200

    removidos arquivos.

--- prime_factors_of_a_number.py
+++ prime_factors_of_a_number.py
@@ -1,6 +1,19 @@
+from math import sqrt
+
+def is_prime(number):
+  if number < 2:
+    return False
+  else:
+    count = 0
+    for iterator in xrange(1, int(sqrt(number))+1):
+      if number % iterator == 0:
+        count += 1
+  return count <= 1
+
 def primes_below(number):
+  root = int(sqrt(number))
   primes = list(xrange(2, number))
-  for iterator in primes:
+  for iterator in xrange(2, root+1):
     for jumper in primes:
       if (jumper % iterator == 0) and (jumper != iterator):
        primes.remove(jumper)
@@ -8,11 +21,14 @@ def primes_below(number):
 
 def prime_factors(number):
   factors = []
-  list_of_primes_below = primes_below(number)
-  for iterator in list_of_primes_below:
-    while number % iterator == 0:
-      number /= iterator
-      factors.append(iterator)
+  if not is_prime(number):
+    list_of_primes_below = primes_below(number)
+    for iterator in list_of_primes_below:
+      while number % iterator == 0:
+        number /= iterator
+        factors.append(iterator)
+  else:
+    factors.append(number)
   return factors
 
 def largest_prime_factor(number):
--- prime_factors_of_a_number_spec.py
+++ prime_factors_of_a_number_spec.py
@@ -15,8 +15,11 @@ class PrimeFactorsSpec(unittest.TestCase):
   def test_the_largest_prime_factor_of_13195_is_29(self):
     largest_prime_factor(13195) |should| equal_to(29)
 
-  def test_the_largest_prime_factor_of_600851475143_is_x(self):
-    largest_prime_factor(600851475143) |should| equal_to(0)
+  def test_the_largest_prime_factor_of_7_is_7(self):
+    largest_prime_factor(7) |should| equal_to(7)
+
+#  def test_the_largest_prime_factor_of_600851475143_is_x(self):
+ #   largest_prime_factor(600851475143) |should| equal_to(0)
 
 if __name__ == '__main__':
   unittest.main()
