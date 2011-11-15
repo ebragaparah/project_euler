c5f8cf873eb1c934d4f9a45a4e162e1ea2742953
= <ebfjunior@gmail.com>
2011-11-15 12:32:16 -0200

    refactoring!

--- prime_factors_of_a_number.py
+++ prime_factors_of_a_number.py
@@ -1,35 +1,28 @@
 from math import sqrt
 
 def is_prime(number):
-  if number < 2:
+  root = int(sqrt(abs(number)))
+  if number == 0 or number == 1:
     return False
   else:
-    count = 0
-    for iterator in xrange(1, int(sqrt(number))+1):
+    counter = 0
+    for iterator in xrange(1, root + 1):
       if number % iterator == 0:
-        count += 1
-  return count <= 1
+        counter += 1
+  return counter <= 1
 
-def primes_below(number):
-  root = int(sqrt(number))
-  primes = list(xrange(2, number))
-  for iterator in xrange(2, root+1):
-    for jumper in primes:
-      if (jumper % iterator == 0) and (jumper != iterator):
-       primes.remove(jumper)
-  return primes
-
-def prime_factors(number):
+def prime_factors_of(number):
+  root = int(sqrt(abs(number)))
   factors = []
   if not is_prime(number):
-    list_of_primes_below = primes_below(number)
-    for iterator in list_of_primes_below:
-      while number % iterator == 0:
-        number /= iterator
-        factors.append(iterator)
+    for iterator in xrange(1, root + 1):
+      if is_prime(iterator):
+        while number % iterator == 0:
+          number /= iterator
+          factors.append(iterator)
   else:
     factors.append(number)
   return factors
 
-def largest_prime_factor(number):
-  return prime_factors(number)[-1]
+def largest_prime_factor_of(number):
+  return prime_factors_of(number)[-1]
