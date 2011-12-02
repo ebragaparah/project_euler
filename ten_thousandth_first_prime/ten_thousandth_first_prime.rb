c44ea424dc36a4a7efd52017203db266ce6d1b70
= <ebfjunior@gmail.com>
2011-12-02 15:10:31 -0200

    retirado método que checava se um número é primo para a utilização do método prime? da biblioteca mathn.

--- ten_thousandth_first_prime/ten_thousandth_first_prime.rb
+++ ten_thousandth_first_prime/ten_thousandth_first_prime.rb
@@ -1,21 +1,5 @@
+require 'mathn'
 include Math
-
-class Fixnum
-  def prime?
-    divisors = []
-    root = sqrt(self.abs).to_i
-    case self
-    when 0, 1
-      false
-    else
-      (1..(root + 1)).each do |iterator|
-        divisors << iterator if ((self % iterator).zero?) and (self != iterator)
-      end
-      divisors.length <= 1
-    end
-  end
-end
-
 
 def prime_in_position(number)
   primes = []
