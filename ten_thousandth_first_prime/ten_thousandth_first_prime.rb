8988719f7fabfed6866615a2fb9f446fcb05b20b
= <ebfjunior@gmail.com>
2011-12-02 18:02:11 -0200

    retirado include do módulo Math.

--- ten_thousandth_first_prime/ten_thousandth_first_prime.rb
+++ ten_thousandth_first_prime/ten_thousandth_first_prime.rb
@@ -1,13 +1,10 @@
 require 'mathn'
-include Math
 
 def prime_in_position(number)
   primes = []
   counter = 0
   while primes.length <= 10001 do
-    if counter.prime?
-      primes << counter
-    end
+    primes << counter if counter.prime?
     counter += 1
   end
   primes[number]
