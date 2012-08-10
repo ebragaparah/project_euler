0eb1bdba5fdbe7f88f9758d26c36ee84eaf14b73
= <ebfjunior@gmail.com>
2012-08-10 09:56:55 -0300

    tabs transformados em espaços para melhorar a identação.

--- how_many_circular_prime_numbers_are_there_below_1000000/how_many_circular_prime_numbers_are_there_below_1000000.rb
+++ how_many_circular_prime_numbers_are_there_below_1000000/how_many_circular_prime_numbers_are_there_below_1000000.rb
@@ -1,17 +1,17 @@
 require 'mathn'
 
 class Fixnum
-	def circular_prime?
-		self.prime? ?
-			self.to_s.reverse.to_i.prime? ? true : false
-		: false
-	end
+  def circular_prime?
+    self.prime? ?
+      self.to_s.reverse.to_i.prime? ? true : false
+    : false
+  end
 end
 
 def sum_of_circular_prime_numbers_to(number)
-	count = 0
-	(1..number).to_a.each do |number|
-		count += number if number.circular_prime?
-	end
-	count
+  count = 0
+  (1..number).to_a.each do |number|
+    count += number if number.circular_prime?
+  end
+  count
 end
