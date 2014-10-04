9bfd2f05a100943987be0bd60cec8471706c5d85
Eduardo Braga Ferreira Junior <ebfjunior@gmail.com>
2014-10-04 17:32:57 -0300

    Corrigindo algumas poucas coisas em inglês :P

--- how_many_circular_prime_numbers_are_there_below_1000000/how_many_circular_prime_numbers_are_there_below_1000000_spec.rb
+++ how_many_circular_prime_numbers_are_there_below_1000000/how_many_circular_prime_numbers_are_there_below_1000000_spec.rb
@@ -4,7 +4,7 @@ require './how_many_circular_prime_numbers_are_there_below_1000000'
 
 describe 'Finding Circular Prime Numbers.' do
   context 'Testing if a number is prime.' do
-    it 'Should returns true if a number is prime.' do
+    it 'Should return true if a number is prime.' do
       0.prime?.should == false
       2.prime?.should == true
       27.prime?.should == false
@@ -13,7 +13,7 @@ describe 'Finding Circular Prime Numbers.' do
   end
 
   context 'Shows if a number is a circular prime number or not.' do
-    it 'Should returns true if a number is a circular prime.' do
+    it 'Should return true if a number is a circular prime.' do
       2.circular_prime?.should == true
       6.circular_prime?.should == false
       13.circular_prime?.should == true
@@ -21,27 +21,27 @@ describe 'Finding Circular Prime Numbers.' do
   end
 
   context 'Shows how many circular prime numbers the are below some number.' do
-    it 'Should returns 2 for the count of the circular primes from 1 to 3' do
+    it 'Should return 2 for the count of the circular primes from 1 to 3' do
       how_many_circular_prime_numbers_to?(3).should == 2
     end
 
-    it 'Should returns 3 for the count of the circular primes from 1 to 5' do
+    it 'Should return 3 for the count of the circular primes from 1 to 5' do
       how_many_circular_prime_numbers_to?(5).should == 3
     end
 
-    it 'Should returns 5 for the count of the circular primes from 1 to 11' do
+    it 'Should return 5 for the count of the circular primes from 1 to 11' do
       how_many_circular_prime_numbers_to?(11).should == 5
     end
 
-    it 'Should returns 13 for the count of the circular primes from 1 to 100' do
+    it 'Should return 13 for the count of the circular primes from 1 to 100' do
       how_many_circular_prime_numbers_to?(100).should == 13
     end
 
-    it 'Should returns 11297 for the count of the circular primes from 1 to 1000000' do
+    it 'Should return 11297 for the count of the circular primes from 1 to 1000000' do
       how_many_circular_prime_numbers_to?(1000000).should == 11297
     end
 
-    it 'Should returns 11297 for the count of the circular primes below 1000000' do
+    it 'Should return 11297 for the count of the circular primes below 1000000' do
       how_many_circular_prime_numbers_to?(999999).should == 11297
     end
   end
